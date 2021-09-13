49f589a69abbf0cd5b1514a87dc97dda73bfbf2c
alemor026 <smoralesb6@miumg.edu.gt>
2021-09-12 21:29:12 -0600

    calculadora java

---
+++ src/Vista/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+/**
+ *
+ * @author CARLOS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         frm_calculadora cal = new frm_calculadora();
+        cal.show();
+    }
+}
